c65cfcf9ff5f5d5e951896433cec01476f58b154
Intel <Intel@YAHWEH>
2020-03-26 20:41:27 +0530

    my first commit.

---
+++ Sample/src/main/java/org/cap/boot/MainClass.java
@@ -0,0 +1,12 @@
+package org.cap.boot;
+
+import org.cap.model.SalesClass;
+
+public class MainClass 
+{
+		public static void main(String args[])
+		{
+			SalesClass obj=new SalesClass();
+			obj.println();
+		}
+}
---
+++ Sample/src/main/java/org/cap/model/SalesClass.java
@@ -0,0 +1,11 @@
+package org.cap.model;
+
+public class SalesClass 
+{
+	int arr[]= {23,45,67,-90,2,1};
+	public void println()
+	{
+		for(int i=0;i<arr.length;i++)
+			System.out.println(arr[i]);
+	}
+}
